65c0f2c5018443fab20e092062e5911dfff3fe60
Alberto Besana <abesana@amaris.com>
2013-08-18 02:41:43 +0200

    [FEATURE] retrieved description

--- scripts/fetcher.py
+++ scripts/fetcher.py
@@ -13,49 +13,73 @@ def fetch(url):
     if g.code != NOT_FOUND:
         result = g.read()
     g.close()
-    open(url.split("/")[-1], "w").write(result)
     return result
 
 def _facebook_like_element(tag):
-    return (tag.name == 'meta') and (
-        tag.has_attr('name') and
-        tag['name'] == 'description')
+    '''<meta name="description" content="Rihanna, Los Angeles, CA. 74,553,559 likes &#xb7; 553,844 talking about this."'''
+    return (tag.name == 'meta' and tag.has_attr('name') and
+             'description' in tag['name'] and tag.has_attr('content'))
 
 def _twitter_followers_element(tag):
     '''<a class="js-nav" href="/pepe/followers" data-element-term="follower_stats" data-nav='followers'>  
   <strong>1,548</strong> Followers
 </a>'''
-    return (tag.name == 'a') and (
-        tag.has_attr("data-element-term")) and (
-        tag["data-element-term"] == "follower_stats")
+    return (tag.name == 'a' and 
+            tag.has_attr("data-element-term") and 
+            tag["data-element-term"] == "follower_stats")
 
-def _extract_like(html_string):
+def _twitter_description_element(tag):
+    '''<meta name="description" content="The latest from Beyonc Knowles (@Beyonce).">'''
+    return (tag.name == 'meta' and tag.has_attr("name") and
+            "description" in tag["name"] and tag.has_attr('content'))
+
+def _facebook_description_element(tag):
+    '''<meta property="og:description" content="http://www.rihanna7.com" />'''
+    return (tag.name == 'meta' and tag.has_attr("property") and
+            'og:description' in tag['property'] and tag.has_attr('content'))
+     
+ 
+def _get_count(content):
+    for word in content.encode("ASCII", 'ignore').split():
+        word = word.replace(",", "").replace(".", "")
+        if word.isdigit():
+            return int(word)
+
+    
+
+def _facebook_extract(html_string):
     "We assume the first integer literal corresponds to number of likes"
-    soup = BeautifulSoup(html_string)
-    for content in (tag["content"] for tag in soup.find_all(
-        _facebook_like_element)):
-        for word in (w.replace(",", "").replace(".", "") for w in
-                     content.encode("ASCII", 'ignore').split()):
-            if word.isdigit():
-                return int(word)
+    result = dict(description=None, count=None)
+    soup = BeautifulSoup(html_string)    
+    elem = soup.find(_facebook_like_element)
+    if elem and elem['content']:
+        result['count'] = _get_count(elem['content'])
+    elem = soup.find(_facebook_description_element)
+    # if elem and elem.child.name == 'div':
+    #     result['description'] = elem and _normalize(elem.child.text)
+    result['description'] = elem and _normalize(elem['content'])
+    return result
 
-def _extract_followers(html_string):
-    '''<a class="js-nav" href="/pepe/followers" data-element-term="follower_stats" data-nav='followers'>  
-  <strong>1,548</strong> Followers
-</a>'''
+def _normalize(unicode_text):
+    "Convert white spaces in space and ignore encoding"
+    return " ".join(unicode_text.encode("ASCII", "ignore").split()) or None
+    
+def _twitter_extract(html_string):
+    result = dict(description=None, count=None)
     soup = BeautifulSoup(html_string)
-    for content in (tag.text for tag in soup.find_all(
-        _twitter_followers_element)):
-        for word in (w.replace(",", "").replace(".", "") for w in
-                     content.encode("ASCII", 'ignore').split()):
-            if word.isdigit():
-                return int(word)
+    elem = soup.find(_twitter_followers_element)
+    if elem and elem.text:
+        count = _get_count(elem.text)
+        result['count'] = count
+    elem = soup.find(_twitter_description_element)
+    result['description'] = elem and _normalize(_normalize(elem['content']))
+    return result
     
 def facebook(username):
-    return _extract_like(fetch(FACEBOOK_URL + quote(username)))
+    return _facebook_extract(fetch(FACEBOOK_URL + quote(username)))
 
 def twitter(username):
-    return _extract_followers(fetch(TWITTER_URL + quote(username)))
+    return _twitter_extract(fetch(TWITTER_URL + quote(username)))
     
 if __name__ == "__main__":
     for username in sys.argv[1:]:
--- scripts/test/parseTests.py
+++ scripts/test/parseTests.py
@@ -6,21 +6,26 @@ from nose.tools import eq_
 DIR = os.path.dirname(__file__)
 
 def test_empty_facebook():
-    got = fetcher._extract_like(
+    got = fetcher._facebook_extract(
         open(os.path.join(DIR, "data", "alberto.besana")).read())
-    eq_(None, got)
+    eq_(None, got['count'])
+    eq_(None, got['description'])
 
 def test_empty_twitter():
-    got = fetcher._extract_like(
+    got = fetcher._twitter_extract(
         open(os.path.join(DIR, "data", "alberto.besana")).read())
-    eq_(None, got)
+    eq_(None, got['count'])
+    eq_(None, got['description'])
 
 def test_facebook():
-    got = fetcher._extract_like(
+    got = fetcher._facebook_extract(
         open(os.path.join(DIR, "data", "cristiano")).read())
-    eq_(59704825, got)
+    eq_(59704825, got['count'])
+    eq_('TEAM HISTORY: Current Club: Real Madrid Number: 7 Youth career 19931995 -- Andorinha...', got['description'])
 
 def test_twitter():
-    got = fetcher._extract_followers(
+    got = fetcher._twitter_extract(
         open(os.path.join(DIR, "data", "beyonce")).read())
-    eq_(11085008, got)
+    eq_(11085008, got['count'])
+    eq_('Lo ltimo de Beyonc Knowles (@Beyonce).', got['description'])
+
